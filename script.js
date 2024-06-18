47f09dee378af199877bc00610ae0076a4786cc8
Ramazi <sazuashviliramazi@gmail.com>
2024-06-18 20:48:05 +0400

    Desktop Visual Done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const emailError = document.querySelector('#email-error');
 function EmailCheck () {
     if(!emailInput.value.match(/^[^ ]+@[^ ]+.[a-z]{2,3}$/)) {
         emailInput.style.backgroundColor="hsl(4, 100%, 67%)";
-        emailError.style.visibility="visible";
+        emailError.style.visibility = "visible";
     }
     
     else{
